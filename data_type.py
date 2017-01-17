2ffc11495f4193200343592cf3bfab8cd0ab40f4
Andrew Young <andrewyang56@gmail.com>
2017-01-17 19:50:46 +0300

    First commit

---
+++ data_type.py
@@ -0,0 +1,26 @@
+def data_type(x=None):
+    if x is None:
+        return "no value"
+
+    elif type(x) == str:
+        return len(x)
+
+    elif type(x) == bool:
+        return x
+
+    elif type(x) == int:
+        if x < 100:
+            return "less than 100"
+
+        elif x > 100:
+            return "more than 100"
+
+        else:
+            return "equal to 100"
+    elif type(x) == list:
+        if len(x) >= 3:
+            return x[2]
+
+        else:
+            return None
+
