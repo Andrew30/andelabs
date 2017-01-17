e6a859d42255ca2a5dc22ede9f355df34b73648f
Andrew Young <andrewyang56@gmail.com>
2017-01-17 20:05:58 +0300

    a function fizz_buzz to return 'Fizz', 'Buzz', 'FizzBuzz', or the argument it receives

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+def fizz_buzz(arg1):
+    if (arg1%3==0) and (arg1%5==0):
+        return 'FizzBuzz'
+    elif (arg1%3==0) and (arg1%5!=0):
+        return 'Fizz'
+    elif (arg1%5==0) and (arg1%3!=0):
+        return 'Buzz'
+    else :
+        return arg1
+
+fizz_buzz(arg1=10)
